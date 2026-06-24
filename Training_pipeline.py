3a3569f0097f188201ccbeaf84846fdb923bb395
Nishadh4014 <nishadhshah1249@gmail.com>
2026-06-24 23:48:18 +0530

    evaluation metrics added

--- Training_pipeline.py
+++ Training_pipeline.py
@@ -21,10 +21,10 @@ X = torch.tensor(messages.todense(), dtype=torch.float32)
 Y = torch.tensor(df["spam"].values, dtype=torch.float32).reshape(-1,1) # without reshaping the shape of Y will be (5572,) but we need it to be (5572,1) for making it compitible with the input feature shape.
 
 model = nn.Linear(1000,1)
-loss_fn = nn.MSELoss()
+loss_fn = nn.BCEWithLogitsLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-for epoch in range(1000):
+for epoch in range(5000):
     optimizer.zero_grad()
     Y_pred = model(X)
     loss = loss_fn(Y_pred, Y)
@@ -38,8 +38,19 @@ model.eval()
 
 with torch.no_grad():
     Y_pred = model(X)
-    Y_pred = torch.sigmoid(Y_pred)  # Apply sigmoid to get probabilities
-    print(Y_pred[:10])  # Print the first 10 predictions
+    Y_pred = torch.sigmoid(Y_pred)  # Apply sigmoid to get probabilities, otherwise the output will be in the range of (-inf, inf) and we need it to be in the range of (0,1) for binary classification.
+    
+    accuracy = ((Y_pred > 0.5) == Y).float().mean()  # Calculate accuracy by comparing the predicted labels with the true labels.
+    print(f"Accuracy: {accuracy.item() * 100:.2f}%")
+    sensitivity = ((Y_pred > 0.5) & (Y == 1)).float().sum() / (Y == 1).float().sum()  # Calculate sensitivity (true positive rate)
+    specificity = ((Y_pred <= 0.5) & (Y == 0)).float().sum() / (Y == 0).float().sum()  # Calculate specificity (true negative rate)
+    print(f"Sensitivity: {sensitivity.item() * 100:.2f}%")  
+    print(f"Specificity: {specificity.item() * 100:.2f}%")  
+    precision = ((Y_pred > 0.5) & (Y == 1)).float().sum() / (Y_pred > 0.5).float().sum()  # Calculate precision (positive predictive value)
+    print(f"Precision: {precision.item() * 100:.2f}%")
+
+
+
 
 
 
